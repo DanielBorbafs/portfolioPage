06cb12ce40b70f1c6b23363fb7e19763661fa83a
DanielBorbafs <dev.danielborba@gmail.com>
2023-12-21 05:38:49 -0300

    Refatorado

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-
 const toggleMenu = () => {
-  document.body.classList.toggle("open");
-};
-
-const toggleItem = (element) => {
-  const headers = document.querySelectorAll("article header");
-  for (let header of headers) {
-    header.classList.remove("active");
-    header.nextElementSibling.style.height = "0px";
-  }
-
-  element.classList.add("active");
-
-  const content = element.nextElementSibling;
-
-  const text = content.querySelector("p");
-
-  content.style.height = `${text.clientHeight}px`;
-};
+    document.body.classList.toggle("open");
+  };
+  
+  const toggleItem = (element) => {
+    const headers = document.querySelectorAll("article header");
+    for (let header of headers) {
+      header.classList.remove("active");
+      header.nextElementSibling.style.height = "0px";
+    }
+  
+    element.classList.add("active");
+  
+    const content = element.nextElementSibling;
+  
+    const text = content.querySelector("p");
+  
+    content.style.height = `${text.clientHeight}px`;
+  };
+  
